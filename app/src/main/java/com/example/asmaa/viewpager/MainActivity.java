8cafd61e0cff12d8710a8bf7556746f73564d134
Asmaamohamed81 <smasimo81@gmail.com>
2018-09-19 11:14:17 +0200

    c

--- app/src/main/java/com/example/asmaa/viewpager/MainActivity.java
+++ app/src/main/java/com/example/asmaa/viewpager/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.asmaa.viewpager;
 
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
         pager = findViewById(R.id.pager);
         tab.setupWithViewPager(pager);
 
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() !=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
         AllFragment fragment1 = AllFragment.getInstance(getlistdata1());
         AllFragment fragment2 = AllFragment.getInstance(getlistdata2());
         AllFragment fragment3 = AllFragment.getInstance(getlistdata3());
@@ -52,10 +60,38 @@ public class MainActivity extends AppCompatActivity {
 
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
 
-        collapsingToolbarLayout.setTitle("djkskdjs");
-        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
 
 
+
+
+
+
+
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
+
+        // hiding & showing the title when toolbar expanded & collapsed
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle(getString(R.string.app_name1));
+                    collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.colorAccent));
+                    collapsingToolbarLayout.setContentScrimColor(getResources().getColor(R.color.colorAccent));
+
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbarLayout.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
 
 
     }
